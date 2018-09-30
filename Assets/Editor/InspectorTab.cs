bb0394d9c41136340cac4019d615097ef00c467f
Luna <malalunapg@gmail.com>
2018-09-30 13:12:24 -0400

    Added DropDown on Inspector GUI for CreateComponent script

---
+++ Assets/Editor/InspectorTab.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (CreateComponenent))]
+public class InspectorTab:  Editor {
+
+    protected int choice = 0;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI ( );
+        CreateComponenent createCompoEditor = (CreateComponenent)target;
+       choice = EditorGUILayout.Popup( choice, createCompoEditor.names);
+    }
+
+}
---
+++ Assets/app/CreateComponenent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateComponenent : MonoBehaviour {
+
+   public  string [] names = new string [] { "Button", " Checkbox", " Slider", "Dropdown", "Toggle" };
+ //   public enum  names { Button,  Checkbox,  Slider, Dropdown, Toggle };
+
+    protected List<GameObject> component ;
+	// Use this for initialization
+	void Start () {
+        //component.Add (new Button);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
